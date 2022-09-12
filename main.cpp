e2b868af86adf2169e66d1e2d24a9d58444c8c4a
Zsigmond02 <71819943+Zsigmond02@users.noreply.github.com>
2022-09-12 18:27:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
+    int *b = new int[NELEMENTS];            //N_ELEMENTS 
     std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    for (int i = 0;) // Hiányzik a végső érték amit az i felvehet és a léptetés mértéke
     {
-        b[i] = i * 2;
+        b[i] = i * 2;  //(i+1)*2 kell írni
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++) // ez a for ciklus nem kell, belemehet a másikba
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" // hiányzik: << b[i]
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag;                               //kezdőértéket kell megadni, illetve átlagot double-el szokták számolni, mert ritkán egész az eredmény (mint itt)
+    for (int i = 0; i < N_ELEMENTS, i++)  // ; kell , helyett
     {
-        atlag += b[i]
+        atlag += b[i]                     //hiányzik a ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
